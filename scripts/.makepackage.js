bc3bf322247edee1f8f0f14f99dd7ed15f8a5429
A-JIE <a-jie.cn@msn.com>
2017-05-15 22:15:01 +0800

    Update make package.
    Update make package.

--- scripts/.makepackage.js
+++ scripts/.makepackage.js
@@ -4,13 +4,26 @@ let path = require('path');
 
 delete pkg.scripts;
 
-let newPkg = Object.assign({}, pkg, {
+let configs = [{
     name: 'anix',
-    main: 'index.js',
-    typings: 'index.d.ts'
-});
+    path: './dist/anix/'
+}, {
+    name: 'react-anix',
+    path: './dist/react/'
+}];
 
-let url = '../dist/anix/';
+for (let i = 0; i < configs.length; i++) {
+    clonePkg(configs[i]);
+}
 
-fs.writeFileSync(url + 'package.json', JSON.stringify(newPkg, null, 2));
-fs.writeFileSync(url + 'README.md', fs.readFileSync('./README.md').toString());
+function clonePkg(config) {
+    let newPkg = Object.assign({}, pkg, {
+        name: config.name,
+        main: 'index.js',
+        typings: 'index.d.ts'
+    });
+
+    let url = config.path;
+    fs.writeFileSync(url + 'package.json', JSON.stringify(newPkg, null, 2));
+    fs.writeFileSync(url + 'README.md', fs.readFileSync('./README.md').toString());
+}
