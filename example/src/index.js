4e20d0c6a5231c22529a2746ef798ed525529748
ajie <yajieliu@tencent.com>
2017-05-17 20:02:19 +0800

    update package
    update package

--- example/src/index.js
+++ example/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
 import '../css/normalize.css';
 import '../css/solarized-dark.css';
 import '../css/styles.css';
