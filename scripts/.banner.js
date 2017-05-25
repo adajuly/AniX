366a93126bf4172ccc8fe6d8fb9579829ac19c33
A-JIE <a-jie.cn@msn.com>
2017-05-25 08:44:52 +0800

    Update any file
    Update any file

---
+++ scripts/.banner.js
@@ -0,0 +1,21 @@
+let pkg = require('../package.json');
+let prependFile = require('prepend-file');
+
+let banner = (name, v) => {
+    return `/**
+* ${name} - VERSION(${v}) - https://github.com/a-jie/AniX
+*/
+`
+};
+
+prependFile('./dist/jq/anix.jq.js', banner('anix.jq', pkg.version), function(err) {
+    if (!err) {
+        console.log('banner added');
+    }
+});
+
+prependFile('./dist/umd/anix.umd.js', banner('anix.umd', pkg.version), function(err) {
+    if (!err) {
+        console.log('banner added');
+    }
+});
