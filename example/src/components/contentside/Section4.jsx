39de63383fd0c6c1487aa66fde7ebf1c2d961af9
a-jie <a-jie.cn@msn.com>
2018-04-11 13:52:43 +0800

    add new case code
    add new case code

--- example/src/components/contentside/Section4.jsx
+++ example/src/components/contentside/Section4.jsx
@@ -6,17 +6,58 @@ import { AniX } from '../../../../dist/umd/anix.umd';
 
 export class Section4 extends Component {
 
+    code1 = `
+// react simple case 
+export class Button extends Component {
+
+    constructor(...args) {
+        super(...args);
+        this.mosueOver = this.mosueOver.bind(this);
+        this.mosueOut = this.mosueOut.bind(this);
+    }
+
+    mosueOver() {
+        AniX.to(this.refs.title, .3, { color: '#ffffff', ease: AniX.ease.linear });
+        AniX.to(this.refs.bg, .3, { y: -50 });
+        AniX.to(this.refs.btn, .3, { scale: 1.05 });
+    }
+    
+    mosueOut() {
+        AniX.to(this.refs.title, .2, { color: '#000000', ease: AniX.ease.linear });
+        AniX.to(this.refs.bg, .25, { y: 0, ease: AniX.ease.linear });
+        AniX.to(this.refs.btn, .3, { scale: 1 });
+    }
+    
+    render() {
+        return (
+            <div ref='btn' onMouseOver={this.mosueOver} onMouseOut={this.mosueOut}>
+                <div className="button-title" ref='title'>{this.props.children}</div>
+                <div className="button-bg" ref='bg'></div>
+            </div>
+        );
+    }
+}
+`;
+
+    code2 = `
+if (type == 'over') {
+    AniX.fromTo(tips, .6, { y: -58 }, { x: 0, y: -38, opacity: 1 });
+} else {
+    AniX.to(tips, .35, { y: -58, opacity: 0 });
+}
+`;
+
     mouseover(id, type) {
         const tips = ReactDOM.findDOMNode(this.refs[id]);
 
         if (id === 'tip1') {
-            if (type == 'over') {
+            if (type === 'over') {
                 AniX.fromTo(tips, .4, { x: 300, y: -38 }, { x: 0, y: -38, opacity: 1, ease: AniX.ease.easeOutBack });
             } else {
                 AniX.to(tips, .35, { x: 300, y: -38, opacity: 0, ease: AniX.ease.easeInBack });
             }
         } else {
-            if (type == 'over') {
+            if (type === 'over') {
                 AniX.fromTo(tips, .6, { y: -58 }, { x: 0, y: -38, opacity: 1 });
             } else {
                 AniX.to(tips, .35, { y: -58, opacity: 0 });
@@ -39,20 +80,30 @@ export class Section4 extends Component {
                             <MyButton>HELLO</MyButton>
                             <MyButton>HELLO</MyButton>
                         </div>
+
+                        <div style={{ clear: 'both' }}>
+                            <pre><code className="javascript">{this.code1}</code></pre>
+                        </div>
                     </div>
+
 
                     <div className="sec-con">
                         <h4 className="blur">mouseover tips</h4>
+
                         <div>
                             <div className="sec-sub-con">
                                 <MyButton styles={{ width: '200px' }} mover={this.mouseover.bind(this, 'tip1')}>please mouseover me!</MyButton>
                                 <Tips ref='tip1'>hello world one!</Tips>
                             </div>
 
-                            <div className="sec-sub-con">
+                            <div style={{ marginLeft: 50 }} className="sec-sub-con">
                                 <MyButton styles={{ width: '200px' }} mover={this.mouseover.bind(this, 'tip2')}>please mouseover me!</MyButton>
                                 <Tips ref='tip2'>hello world two!</Tips>
                             </div>
+                        </div>
+
+                        <div style={{ clear: 'both' }}>
+                            <pre><code className="javascript">{this.code2}</code></pre>
                         </div>
                     </div>
                 </div>
--- example/src/components/contentside/demo/MyButton.jsx
+++ example/src/components/contentside/demo/MyButton.jsx
@@ -4,17 +4,19 @@ import './MyButton.css';
 
 export class MyButton extends Component {
 
-    mosueover() {
+    mosueover(e) {
         AniX.to(this.refs.txt, .3, { color: '#ffffff', ease: AniX.ease.linear });
         AniX.to(this.refs.bg, .3, { y: -50 });
         AniX.to(this.refs.con, .3, { scale: 1.05 });
+
         this.props.mover && this.props.mover('over');
     }
 
-    mosueout() {
+    mosueout(e) {
         AniX.to(this.refs.txt, .2, { color: '#000000', ease: AniX.ease.linear });
         AniX.to(this.refs.bg, .25, { y: 0, ease: AniX.ease.linear });
         AniX.to(this.refs.con, .3, { scale: 1 });
+
         this.props.mover && this.props.mover('out');
     }
 
